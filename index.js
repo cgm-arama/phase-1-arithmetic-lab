fbb06c236badd2fec76a1bc2df196f233406c9ec
Eric <eric.arama@student.moringaschool.com>
2023-03-14 10:58:25 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
 
+console.log(multiply)
+
+
+let random = Math.round(Math.random())+1
+
+console.log(random)
+
+let num3 = 436;
+let num4 = 54
+let mod = num3 % num4
+
+console.log(mod)
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+
+console.log(max)
 // Write your code here
